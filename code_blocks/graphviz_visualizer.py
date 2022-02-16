2eda5713f122a413ff4591cfdcd2a8fdc9cc7aab
Tom Selfin <selfint@gmail.com>
2022-02-16 19:09:40 +0200

    FEAT: add visualizer

---
+++ code_blocks/graphviz_visualizer.py
@@ -0,0 +1,90 @@
+from collections import defaultdict
+from pathlib import Path
+import graphviz
+from code_blocks.types import Definition, Reference, ResolvedReference
+from typing import DefaultDict, Dict, List, Optional, Set, Tuple
+
+
+class GraphvizVisualizer:
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def definition_to_id(definition: Definition) -> str:
+        return "#".join(
+            ["#".join(definition.path), "#".join(definition.scope), definition.name]
+        )
+
+    @staticmethod
+    def reference_to_id(reference: Reference) -> str:
+        return "#".join(
+            [
+                "#".join(reference.path),
+                "#".join(reference.scope),
+            ]
+        )
+
+    def visualize(
+        self,
+        definitions: Set[Definition],
+        references: Set[Reference],
+        resolved_references: Set[ResolvedReference],
+        output: Optional[Path] = None,
+    ):
+        g = graphviz.Digraph("G", filename=output, engine="fdp")
+
+        files: DefaultDict[
+            Tuple[str, ...], Tuple[Set[Definition], Set[Reference]]
+        ] = defaultdict(lambda: (set(), set()))
+        for d in definitions:
+            files[d.path][0].add(d)
+        for r in references:
+            files[r.path][1].add(r)
+
+        for path, (definitions, references) in files.items():
+            path_str = "_".join(path)
+            subgraph = graphviz.Digraph(f"cluster_{path_str}")
+            subgraph.attr("graph", rankdir="LR")
+            subgraph.attr("graph", label=path_str)
+
+            for definition in definitions:
+                label = (
+                    "#".join(
+                        [
+                            "#".join(definition.path),
+                            "#".join(definition.scope),
+                        ]
+                    )
+                    + "#"
+                    + definition.kind
+                    + " "
+                    + definition.name
+                )
+                subgraph.node(
+                    name=self.definition_to_id(definition),
+                    label=label,
+                )
+
+            for reference in references:
+                label = "#".join(
+                    [
+                        "#".join(reference.path),
+                        "#".join(reference.scope),
+                    ]
+                )
+                subgraph.node(
+                    name=self.reference_to_id(reference),
+                    label=label,
+                )
+
+            g.subgraph(subgraph)
+
+        for resolved_reference in resolved_references:
+            if resolved_reference is None:
+                continue
+            g.edge(
+                tail_name=self.reference_to_id(resolved_reference.reference),
+                head_name=self.definition_to_id(resolved_reference.definition),
+            )
+
+        g.view()
