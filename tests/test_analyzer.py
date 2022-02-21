bfd8575c3e0291098af28c914a9fd42a017cfd2b
Tom Selfin <selfint@gmail.com>
2022-02-21 17:04:01 +0200

    REFACTOR: extract test helper method

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -1,27 +1,25 @@
+from typing import List
 import pandas as pd
 from code_blocks.analyzer import get_depths
 
 
-def test_get_depths():
-    df = pd.DataFrame({"src": [1,2,3], "dst": [2,3,4]})
-    expected = pd.Series([0,1,2])
+def assert_expected_depths(src_col: list, dst_col: list, expected: List[int]):
+    df = pd.DataFrame({"src": src_col, "dst": dst_col})
+    expected = pd.Series(expected)
     actual = get_depths(df, "src", "dst")
 
     pd.testing.assert_series_equal(expected, actual)
+
+
+
+def test_get_depths():
+    assert_expected_depths([1, 2, 3], [2, 3, 4], [0, 1, 2])
 
 
 def test_get_depths_when_loop():
-    df = pd.DataFrame({"src": [1,2,3], "dst": [2,3,1]})
-    expected = pd.Series([0,0,0])
-    actual = get_depths(df, "src", "dst")
-
-    pd.testing.assert_series_equal(expected, actual)
+    assert_expected_depths([1, 2, 3], [2, 3, 1], [0, 0, 0])
 
 
 def test_get_depths_when_also_loop():
-    df = pd.DataFrame({"src": [1,2,3], "dst": [2,3,2]})
-    expected = pd.Series([0,1,1])
-    actual = get_depths(df, "src", "dst")
-
-    pd.testing.assert_series_equal(expected, actual)
+    assert_expected_depths([1, 2, 3], [2, 3, 2], [0, 1, 1])
 
