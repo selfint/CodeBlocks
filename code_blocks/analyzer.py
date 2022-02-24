cf0beb245b07f87863d1e68e5bf1de1b0a13fe20
Tom Selfin <selfint@gmail.com>
2022-02-24 14:07:13 +0200

    FEAT: add analyzer module, calculate node depths

--- code_blocks/analyzer.py
+++ code_blocks/analyzer.py
@@ -1,28 +1,81 @@
+from collections import deque
+from typing import Any, Dict, Optional, Set
 from pandas import DataFrame, Series
 
 
+Graph = Dict[Any, Set[Any]]
+
+
+def _build_graph(df: DataFrame, src_col: str, dst_col: str) -> Graph:
+    graph = dict(df[[src_col, dst_col]].groupby(src_col)[dst_col].groups)
+
+    # convert values to lists instead of series
+    graph = {k: set(df.loc[v, dst_col]) for k, v in graph.items()}
+
+    # assign dst nodes to be leaf nodes if they aren't in the graph
+    for dst_node in df[dst_col].unique():
+        graph[dst_node] = graph.get(dst_node, set())
+
+    return graph
+
+
+def _find_shortest_path(graph, start, end):
+    """
+    Code by Eryk Kopczyński from https://www.python.org/doc/essays/graphs
+
+    Note that this returns the path in a weird format, e.g., [[['A'], 'B'], 'D']
+    """
+
+    dist = {start: [start]}
+    q = deque([start])  # changed to init queue from list
+    while len(q):
+        at = q.popleft()
+        for next in graph[at]:
+            if next not in dist:
+                dist[next] = [dist[at], next]
+                q.append(next)
+
+    return dist.get(end)
+
+
+def _flatten_path(p: list) -> list:
+    """Convert [[['A'], 'B'], 'D'] to ['A', 'B', 'D']"""
+
+    if len(p) == 1:
+        return p
+
+    else:
+        return _flatten_path(p[0]) + [p[1]]
+
+
+def get_distance(graph: Graph, start: Any, end: Any) -> Optional[int]:
+    path = _find_shortest_path(graph, start, end)
+
+    if path is not None:
+        flattened_path = _flatten_path(path)
+
+        # depth is the amount of connections
+        depth = len(flattened_path) - 1
+
+        return depth
+    else:
+        return None
+
+
+def get_node_depth(node: Any, graph: Graph) -> int:
+    return max(
+        get_distance(graph, source, node) or 0
+        for source in graph
+    )
+
+
 def get_depths(df: DataFrame, src_col: str, dst_col: str) -> Series:
-    depths = Series(0, index=df.index)
+    graph = _build_graph(df, src_col, dst_col)
 
-    # find all roots as sources that are never a destination
-    src_is_root = df[src_col].apply(lambda s: s not in df[dst_col].values)
+    depths = Series({n: get_node_depth(n, graph) for n in graph})
 
-    # no roots means everything is at the same depth
-    if not src_is_root.any():
-        return depths
-
-    # get all depths of all non-root connections
-    non_roots: DataFrame = df[~src_is_root]
-    if not non_roots.empty:
-        non_root_depths = get_depths(non_roots, src_col, dst_col)
-
-        # update all depths of non root connections to be relative
-        # to our roots
-        non_root_depths += 1
-
-        # update depths of non root connections
-        depths.update(non_root_depths)
+    # make sure depths are relative to 0
+    depths -= depths.min()
 
     return depths
-
 
--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -1,25 +1,52 @@
-from typing import List
+from typing import Dict
 import pandas as pd
 from code_blocks.analyzer import get_depths
 
 
-def assert_expected_depths(src_col: list, dst_col: list, expected: List[int]):
+def assert_expected_depths(src_col: list, dst_col: list, expected: Dict[int, int]):
     df = pd.DataFrame({"src": src_col, "dst": dst_col})
-    expected = pd.Series(expected)
-    actual = get_depths(df, "src", "dst")
+    actual = get_depths(df, "src", "dst").to_dict()
 
-    pd.testing.assert_series_equal(expected, actual)
+    assert actual == expected
 
 
 
 def test_get_depths():
-    assert_expected_depths([1, 2, 3], [2, 3, 4], [0, 1, 2])
+    assert_expected_depths(
+        [1, 2, 3],
+        [2, 3, 4],
+        {1: 0, 2: 1, 3: 2, 4: 3},
+    )
 
 
 def test_get_depths_when_loop():
-    assert_expected_depths([1, 2, 3], [2, 3, 1], [0, 0, 0])
+    assert_expected_depths(
+        [1, 2, 3],
+        [2, 3, 1],
+        {1: 0, 2: 0, 3: 0},
+    )
 
 
 def test_get_depths_when_also_loop():
-    assert_expected_depths([1, 2, 3], [2, 3, 2], [0, 1, 1])
+    assert_expected_depths(
+        [1, 2, 3],
+        [2, 3, 2],
+        {1: 0, 2: 1, 3: 2},
+    )
+
+
+def test_get_depths_multiple_connections_per_node():
+    assert_expected_depths(
+        [1, 2, 2],
+        [2, 3, 4],
+        {1: 0, 2: 1, 3: 2, 4: 2},
+    )
+
+
+def test_get_depths_multiple_connections_per_node_and_loop():
+    assert_expected_depths(
+        [1, 2, 2],
+        [2, 1, 3],
+        {1: 0, 2: 0, 3: 1},
+    )
 
